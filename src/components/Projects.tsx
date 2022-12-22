75db6269c5f9b57e3ea0ed7915dbf7a65c5a7813
Matias <matiasdonato14@gmail.com>
2022-12-22 14:19:35 -0300

    portfolio completed

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,8 +1,12 @@
 import "../css/portfolio.css"
 import dogsAppImage from "../assets/images/DogsApp.png"
+import nftradeImage from "../assets/images/nftrade.png"
+import weatherAppImage from "../assets/images/WeatherApp.png"
+import blackCloverTattooImage from "../assets/images/BlackCloverTattoo.png"
 import SvgInfo from "../assets/icons/svgInfo"
 import SvgOpenWindow from "../assets/icons/svgOpenWindow"
 import SvgGitHub from "../assets/icons/svgGitHub"
+
 
 export default function Projects(){
     return (
@@ -21,46 +25,23 @@ export default function Projects(){
                       <h3>Dogs App</h3>
                     </div>
                     <div className="projectInfoContainer">
-                      <div className="projectLinkContainer">
-                        <p>Deploy</p>
-                        <a href="https://dogs-app-matiasdonato.vercel.app" target={"_blank"} rel="noreferrer">
+                      <a href="https://dogs-app-matiasdonato.vercel.app" target={"_blank"} rel="noreferrer">
+                        <div className="projectLinkContainer">
+                          <p>Deploy</p>
                           <SvgOpenWindow className="projectInfoIcon" />
-                        </a>
-                      </div>
-                      <div className="projectLinkContainer">
-                        <p>Info</p>
-                        <a href="https://www.linkedin.com/in/matías-donato-227488224/details/projects/" target={"_blank"} rel="noreferrer">
+                        </div>
+                      </a>
+                      <a href="https://www.linkedin.com/in/matías-donato-227488224/details/projects/" target={"_blank"} rel="noreferrer">
+                        <div className="projectLinkContainer">
+                          <p>Info</p>
                           <SvgInfo className="projectInfoIcon" />
-                        </a>
-                      </div>
-                      <div className="projectLinkContainer">
-                        <p>Github</p>
-                        <a href="https://github.com/matiasdonato/Dogs-App--PI-Henry" target={"_blank"} rel="noreferrer">
+                        </div>
+                      </a>
+                      <a href="https://github.com/matiasdonato/Dogs-App--PI-Henry" target={"_blank"} rel="noreferrer">
+                        <div className="projectLinkContainer">
+                          <p>Github</p>
                           <SvgGitHub className="projectInfoIcon" />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div className="projectContainer">
-              <a href="https://dogs-app-matiasdonato.vercel.app" target={"_blank"} rel="noreferrer">
-                <img src={dogsAppImage} alt="Dogs App Matias Donato" className="projectImage" />
-                <div className="projectInfo">
-                  <div className="mainProjectInfo">
-                    <div className="projectTitleContainer">
-                      <h3>Dogs App</h3>
-                      <a href="https://dogs-app-matiasdonato.vercel.app" target={"_blank"} rel="noreferrer">
-                        <SvgOpenWindow className="projectLinkIcon" />
-                      </a>
-                    </div>
-                    <div>
-                      <a href="https://www.linkedin.com/in/matías-donato-227488224/details/projects/" target={"_blank"} rel="noreferrer">
-                        <SvgInfo className="projectInfoIcon" />
-                      </a>
-                      <a href="https://github.com/matiasdonato/Dogs-App--PI-Henry" target={"_blank"} rel="noreferrer">
-                        <SvgGitHub className="projectInfoIcon" />
+                        </div>
                       </a>
                     </div>
                   </div>
@@ -68,22 +49,31 @@ export default function Projects(){
               </a>
             </div>
             <div className="projectContainer">
-              <a href="https://dogs-app-matiasdonato.vercel.app" target={"_blank"} rel="noreferrer">
-                <img src={dogsAppImage} alt="Dogs App Matias Donato" className="projectImage" />
+              <a href="https://pf-nftrade.netlify.app" target={"_blank"} rel="noreferrer">
+                <img src={nftradeImage} alt="Dogs App Matias Donato" className="projectImage" />
                 <div className="projectInfo">
                   <div className="mainProjectInfo">
                     <div className="projectTitleContainer">
-                      <h3>Dogs App</h3>
-                      <a href="https://dogs-app-matiasdonato.vercel.app" target={"_blank"} rel="noreferrer">
-                        <SvgOpenWindow className="projectLinkIcon" />
-                      </a>
+                      <h3>NFTRADE</h3>
                     </div>
-                    <div>
-                      <a href="https://www.linkedin.com/in/matías-donato-227488224/details/projects/" target={"_blank"} rel="noreferrer">
-                        <SvgInfo className="projectInfoIcon" />
+                    <div className="projectInfoContainer">
+                      <a href="https://pf-nftrade.netlify.app" target={"_blank"} rel="noreferrer">
+                        <div className="projectLinkContainer">
+                          <p>Deploy</p>
+                          <SvgOpenWindow className="projectInfoIcon" />
+                        </div>
                       </a>
-                      <a href="https://github.com/matiasdonato/Dogs-App--PI-Henry" target={"_blank"} rel="noreferrer">
-                        <SvgGitHub className="projectInfoIcon" />
+                      <a href="https://www.linkedin.com/in/matías-donato-227488224/details/projects/" target={"_blank"} rel="noreferrer">
+                        <div className="projectLinkContainer">
+                          <p>Info</p>
+                          <SvgInfo className="projectInfoIcon" />
+                        </div>
+                      </a>
+                      <a href="https://github.com/NicoCastagnet/PF-NFTRADE" target={"_blank"} rel="noreferrer">
+                        <div className="projectLinkContainer">
+                          <p>Github</p>
+                          <SvgGitHub className="projectInfoIcon" />
+                        </div>
                       </a>
                     </div>
                   </div>
@@ -91,22 +81,63 @@ export default function Projects(){
               </a>
             </div>
             <div className="projectContainer">
-              <a href="https://dogs-app-matiasdonato.vercel.app" target={"_blank"} rel="noreferrer">
-                <img src={dogsAppImage} alt="Dogs App Matias Donato" className="projectImage" />
+              <a href="https://weather-app-matiasdonato.vercel.app" target={"_blank"} rel="noreferrer">
+                <img src={weatherAppImage} alt="Dogs App Matias Donato" className="projectImage" />
                 <div className="projectInfo">
                   <div className="mainProjectInfo">
                     <div className="projectTitleContainer">
-                      <h3>Dogs App</h3>
-                      <a href="https://dogs-app-matiasdonato.vercel.app" target={"_blank"} rel="noreferrer">
-                        <SvgOpenWindow className="projectLinkIcon" />
+                      <h3>Weather App</h3>
+                    </div>
+                    <div className="projectInfoContainer">
+                      <a href="https://weather-app-matiasdonato.vercel.app" target={"_blank"} rel="noreferrer">
+                        <div className="projectLinkContainer">
+                          <p>Deploy</p>
+                          <SvgOpenWindow className="projectInfoIcon" />
+                        </div>
+                      </a>
+                      <a href="https://www.linkedin.com/in/matías-donato-227488224/details/projects/" target={"_blank"} rel="noreferrer">
+                        <div className="projectLinkContainer">
+                          <p>Info</p>
+                          <SvgInfo className="projectInfoIcon" />
+                        </div>
+                      </a>
+                      <a href="https://github.com/matiasdonato/weatherApp" target={"_blank"} rel="noreferrer">
+                        <div className="projectLinkContainer">
+                          <p>Github</p>
+                          <SvgGitHub className="projectInfoIcon" />
+                        </div>
                       </a>
                     </div>
-                    <div>
-                      <a href="https://www.linkedin.com/in/matías-donato-227488224/details/projects/" target={"_blank"} rel="noreferrer">
-                        <SvgInfo className="projectInfoIcon" />
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="projectContainer">
+              <a href="https://black-clover-tattoo.netlify.app" target={"_blank"} rel="noreferrer">
+                <img src={blackCloverTattooImage} alt="Dogs App Matias Donato" className="projectImage" />
+                <div className="projectInfo">
+                  <div className="mainProjectInfo">
+                    <div className="projectTitleContainer">
+                      <h3>Weather App</h3>
+                    </div>
+                    <div className="projectInfoContainer">
+                      <a href="https://black-clover-tattoo.netlify.app" target={"_blank"} rel="noreferrer">
+                        <div className="projectLinkContainer">
+                          <p>Deploy</p>
+                          <SvgOpenWindow className="projectInfoIcon" />
+                        </div>
                       </a>
-                      <a href="https://github.com/matiasdonato/Dogs-App--PI-Henry" target={"_blank"} rel="noreferrer">
-                        <SvgGitHub className="projectInfoIcon" />
+                      <a href="https://www.linkedin.com/in/matías-donato-227488224/details/projects/" target={"_blank"} rel="noreferrer">
+                        <div className="projectLinkContainer">
+                          <p>Info</p>
+                          <SvgInfo className="projectInfoIcon" />
+                        </div>
+                      </a>
+                      <a href="https://github.com/matiasdonato/BlackCloverTattooStudio--HTML-CSS-Project" target={"_blank"} rel="noreferrer">
+                        <div className="projectLinkContainer">
+                          <p>Github</p>
+                          <SvgGitHub className="projectInfoIcon" />
+                        </div>
                       </a>
                     </div>
                   </div>
