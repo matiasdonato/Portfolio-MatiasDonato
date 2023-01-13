f336cbbe4552cccfed6644f0255803af651795a0
Matias <matiasdonato14@gmail.com>
2023-01-13 20:53:17 -0300

    contact modal

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -115,65 +115,76 @@ export default function Contact(){
       body: ""
     })
     setEmailLoading(false)
+    setShowModal(true)
   }
 
+  let [showModal, setShowModal] = useState(false)
+
   return (
-    <div className="bigContactContainer" >
-      <div className="contactTextContainer" >
-        <h2>Contact me!</h2>
-      </div>
-      <div className="contactContainer">
-        <div className="contactMeIconsContainer" >
-          <div className="contactMeIcon">
-            <SvgMail className= "contactMeIconSvg" />
-            <span>matiasdonato14@gmail.com</span>
-          </div>
-          <div className="contactMeIcon">
-            <SvgPhone className= "contactMeIconSvg" />
-            <span>+54 9 11 6209-1233</span>
-          </div>
-          <a href="https://www.linkedin.com/in/matías-donato-227488224/" target={"_blank"} rel="noreferrer" className="contactMeIcon" >
-            <SvgLinkedin className= "contactMeIconSvg" />
-            <span>Matias Donato</span>
-          </a>
-          <a href="https://github.com/matiasdonato" target={"_blank"} rel="noreferrer" className="contactMeIcon">
-            <SvgGitHub className= "contactMeIconSvg" />
-            <span>matiasdonato</span>
-          </a>
+    <div className={`bigContactContainer`} >
+      {showModal === true && 
+        <div className="contactModal">
+          <p>This message was successfully delivered. Thanks for contacting me!</p>
+          <button className="closeModalButton" onClick={() => setShowModal(false)} >Close</button>
         </div>
-        <form className="contactForm" onSubmit={(e) => formSubmit(e)}>
-          <div className="inputBox">
-            <input className="contactInput" autoComplete="off" maxLength={24} type="text" required={true} name="name" value={contactForm.name} onChange={(e) => handleInput(e) } />
-            <span>Name</span>
-            <i></i>
-            <div className="borderInput"></div>
+      }
+      <div className={`${showModal === true && "blurBg"} `} >
+        <div className="contactTextContainer" >
+          <h2>Contact me!</h2>
+        </div>
+        <div className="contactContainer">
+          <div className="contactMeIconsContainer" >
+            <div className="contactMeIcon">
+              <SvgMail className= "contactMeIconSvg" />
+              <span>matiasdonato14@gmail.com</span>
+            </div>
+            <div className="contactMeIcon">
+              <SvgPhone className= "contactMeIconSvg" />
+              <span>+54 9 11 6209-1233</span>
+            </div>
+            <a href="https://www.linkedin.com/in/matías-donato-227488224/" target={"_blank"} rel="noreferrer" className="contactMeIcon" >
+              <SvgLinkedin className= "contactMeIconSvg" />
+              <span>Matias Donato</span>
+            </a>
+            <a href="https://github.com/matiasdonato" target={"_blank"} rel="noreferrer" className="contactMeIcon">
+              <SvgGitHub className= "contactMeIconSvg" />
+              <span>matiasdonato</span>
+            </a>
           </div>
-          {error.name && <p className="errorText">*{error.name}</p> }
-          <div className="inputBox"> 
-            <input className="contactInput" autoComplete="off" maxLength={38} type="text" required={true} name="email" value={contactForm.email} onChange={(e) => handleInput(e) } />
-            <span>Mail</span>
-            <i></i> 
-            <div className="borderInput"></div>
-          </div>
-          {error.email && <p className="errorText" >*{error.email}</p>}
-          <div className="inputBox"> 
-            <input className="contactInput" autoComplete="off" maxLength={34} type="text" required={true} name="subject" value={contactForm.subject} onChange={(e) => handleInput(e) } />
-            <span>Subject</span>
-            <i></i>
-            <div className="borderInput"></div>
-          </div>
-          {error.subject && <p className="errorText" >*{error.subject}</p>}
-          <div className="inputBox"> 
-            <textarea className="contactInput" name="body" required={true} value={contactForm.body} cols={30} rows={10} onChange={(e) => handleInput(e) } maxLength={600} ></textarea>
-            <span>Message</span> 
-            <i></i>
-            <div className="borderTextarea"></div>
-          </div>
-          {error.body && <p className="errorText" >*{error.body}</p>}
-          <div className="submitContactContainer">
-            {emailLoading === false ? <input type="submit" className="submitButton" value={"Submit"} disabled={ error.name !== null || error.email !== null || error.subject !== null || error.body !== null} /> : <div className="loadingContainer" > <img className="loadingSubmit" src={loadingGif} alt="loading" /> </div> }        
-          </div>
-        </form>
+          <form className="contactForm" onSubmit={(e) => formSubmit(e)}>
+            <div className="inputBox">
+              <input className="contactInput" autoComplete="off" maxLength={24} type="text" required={true} name="name" value={contactForm.name} onChange={(e) => handleInput(e) } />
+              <span>Name</span>
+              <i></i>
+              <div className="borderInput"></div>
+            </div>
+            {error.name && <p className="errorText">*{error.name}</p> }
+            <div className="inputBox"> 
+              <input className="contactInput" autoComplete="off" maxLength={38} type="text" required={true} name="email" value={contactForm.email} onChange={(e) => handleInput(e) } />
+              <span>Mail</span>
+              <i></i> 
+              <div className="borderInput"></div>
+            </div>
+            {error.email && <p className="errorText" >*{error.email}</p>}
+            <div className="inputBox"> 
+              <input className="contactInput" autoComplete="off" maxLength={34} type="text" required={true} name="subject" value={contactForm.subject} onChange={(e) => handleInput(e) } />
+              <span>Subject</span>
+              <i></i>
+              <div className="borderInput"></div>
+            </div>
+            {error.subject && <p className="errorText" >*{error.subject}</p>}
+            <div className="inputBox"> 
+              <textarea className="contactInput" name="body" required={true} value={contactForm.body} cols={30} rows={10} onChange={(e) => handleInput(e) } maxLength={600} ></textarea>
+              <span>Message</span> 
+              <i></i>
+              <div className="borderTextarea"></div>
+            </div>
+            {error.body && <p className="errorText" >*{error.body}</p>}
+            <div className="submitContactContainer">
+              {emailLoading === false ? <input type="submit" className="submitButton" value={"Submit"} disabled={ error.name !== null || error.email !== null || error.subject !== null || error.body !== null} /> : <div className="loadingContainer" > <img className="loadingSubmit" src={loadingGif} alt="loading" /> </div> }        
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   )
